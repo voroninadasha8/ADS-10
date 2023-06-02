bbea798e3cef946fed852e5b9e8b3323c0325743
voroninadasha8 <117026926+voroninadasha8@users.noreply.github.com>
2023-06-02 13:45:32 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -7,47 +7,47 @@
 
 class Tree {
  private:
- struct Node {
-  char ch;
-  int tr = 0;
-  std::vector<Node*> vec;
- };
- Node* root;
- std::vector<std::vector<char>> allperms;
- void createallperms(Node* root, std::vector<char> v) {
-  if (!root->tr) {
-   v.push_back(root->ch);
-  }
-  if (!root->vec.empty()) {
-   for (TreeNode* next : root->vec) {
-    createallperms(next, v);
+   struct Node {
+     char ch;
+    int tr = 0;
+    std::vector<Node*> vec;
+   };
+   Node* root;
+   std::vector<std::vector<char>> allperms;
+   void createallperms(Node* root, std::vector<char> v) {
+    if (!root->tr) {
+      v.push_back(root->ch);
+    }
+    if (!root->vec.empty()) {
+      for (TreeNode* next : root->vec) {
+        createallperms(next, v);
+      }
+    } else {
+      allperms.emplace_back(v);
+    }
    }
-  } else {
-   allperms.emplace_back(v);
-  }
- }
  void create(Node* root, const std::vector<char>& in) {
-  for (char ch : in) {
-   Node* temp = new Node;
-   temp->ch = ch;
-   root->vec.emplace_back(temp);
-   std::vector<char> remains = in;
-   remains.erase(std::find(remains.begin(), remains.end(), ch));
-   create(temp, remains);
-  }
+   for (char ch : in) {
+     Node* temp = new Node;
+     temp->ch = ch;
+     root->vec.emplace_back(temp);
+     std::vector<char> remains = in;
+     remains.erase(std::find(remains.begin(), remains.end(), ch));
+     create(temp, remains);
+   }
  }
 
  public:
-  explicit Tree(std::vector<char> in) {
-    root = new Node;
-    root->ch = ' ';
-    root->tr = 1;
-    create(root, in);
-    std::vector<char> v;
-    createallperms(root, v);
-  }
-  std::vector<std::vector<char>> getAllPerms() const {
-    return allperms;
-  }
+   explicit Tree(std::vector<char> in) {
+     root = new Node;
+     root->ch = ' ';
+     root->tr = 1;
+     create(root, in);
+     std::vector<char> v;
+     createallperms(root, v);
+   }
+ std::vector<std::vector<char>> getAllPerms() const {
+   return allperms;
+ }
 };
 #endif  // INCLUDE_TREE_H_
